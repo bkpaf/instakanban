9f4914099ec0bb4803586e92c95f3898dbe4ab0b
bkpaf <bkpaf@hotmail.com>
2021-09-27 21:42:54 +0100

    handle unknown authors

--- js/main.js
+++ js/main.js
@@ -8,12 +8,15 @@ fetch("https://type.fit/api/quotes")
   })
   .then(function (data) {
     let motivational = data;
-
+    console.log(data);
     document.querySelector(".motivational").addEventListener("click", () => {
       let i = Math.floor(Math.random() * 1643 + 1);
       let quote = motivational[i].text;
       let author = motivational[i].author;
 
+      if (!author) {
+        author = "Unknown";
+      }
       document.querySelector(".motivation").textContent = `${quote}`;
       document.querySelector(".author").textContent = `${author}`;
     });
